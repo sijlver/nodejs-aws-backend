28728f22223147fde0952daa762fef67659a8421
Igor Tsialiuk <igor.tsialiuk@burberry.com>
2020-12-31 11:26:22 +0300

    cloudformation-template-example-service: Add docker valumes example

--- cloudformation-template-example-service/index.js
+++ cloudformation-template-example-service/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
 const app = express();
 const port = process.env.PORT || 3000;
+const url = path.join(__dirname, './data/index.txt');
 
-app.get('*', (req, res) => res.send("I'm server!"));
+app.get('/favicon.ico', (req, res) => res.send(''));
+
+app.get('*', async (req, res) => {
+  const data = await readFile(url, { encoding: 'utf-8' }).catch(err => {
+    console.log('Read file: ', err);
+
+    return 1;
+  });
+  const count = Number(data) + 1;
+
+  await writeFile(url, count).catch(console.log);
+
+  res.send(`I'm server! Count: ${count}`);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
